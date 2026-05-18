c489227fa138943099b02335a36ccb23dc77ccbd
ab123445 <a01039317456@gmail.com>
2026-05-18 19:57:45 +0900

    20260518
    지뢰찾기

---
+++ MineSearch/mine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MineSearch
+{
+    internal class mine
+    {
+        public const int X = 60;
+        public const int Y = 60;
+        Button btnMine = new();
+
+        public mine(int x, int y)
+        {
+            btnMine.Location = new Point(x*X, y*Y);
+            btnMine.Name = "btnMine";
+            btnMine.Size = new Size(X, Y);
+            btnMine.TabIndex = 0;
+            btnMine.Text = "a";
+            btnMine.UseVisualStyleBackColor = true;
+        }
+    }
+}
