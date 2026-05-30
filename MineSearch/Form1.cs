f19a9daa7e37442c81989fb6fb17034e097114d7
ab123445 <a01039317456@gmail.com>
2026-05-30 11:56:35 +0900

    2026 0530
    지뢰 좌클릭 우클릭

--- MineSearch/Form1.cs
+++ MineSearch/Form1.cs
@@ -14,7 +14,7 @@ namespace MineSearch
             //ClientRectangle.Width
             this.Width += 9 * Mine.X - ClientRectangle.Width;
             this.Height += 9 * Mine.Y - ClientRectangle.Height;
-            
+
 
             for (int i = 0; i < 9; i++)
             {
@@ -22,8 +22,7 @@ namespace MineSearch
                 for (int j = 0; j < 9; j++)
                 {
                     int r = rand.Next(0, 100);
-
-                    Mine mine = new(i, j);
+                    Mine mine = new(i, j, r, button_MouseDown);
                     Controls.Add(mine);
                     Mines[i].Add(mine);
                 }
@@ -31,5 +30,27 @@ namespace MineSearch
 
         }
 
+        public void button_MouseDown(object sender, MouseEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button == null) return;
+            Mine me = button.Tag as Mine;
+            if (me == null) return;
+
+
+            if (e.Button == MouseButtons.Left)
+            {
+                me.Text_Change();
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                me.Set_Flag();
+            }
+        }
+
+        private void txtRulebox_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
--- MineSearch/mine.cs
+++ MineSearch/mine.cs
@@ -4,25 +4,47 @@ using System.Text;
 
 namespace MineSearch
 {
+    public delegate void MineClick(object sender, MouseEventArgs e);
     public class Mine : Button
     {
         public const int X = 60;
         public const int Y = 60;
-
-        public Mine(int x, int y)
+        bool IsMine;
+        
+        public Mine(int x, int y, int random, MineClick click)
         {
+            this.Tag = this;
+
             this.Location = new Point(x*X, y*Y);
             this.Name = "btnMine";
             this.Size = new Size(X, Y);
             this.TabIndex = 0;
             this.Text = "";
             this.UseVisualStyleBackColor = true;
-            this.Click += Mine_Click;
+            Color Default_color = this.BackColor;
+            this.MouseDown += new MouseEventHandler(click);
+            
+            if (random > 70)
+                IsMine = true;
+            else
+                IsMine = false;
         }
 
-        private void Mine_Click(object sender, EventArgs e)
+        public void Text_Change()
         {
-            this.Text = "X";
+            if (IsMine == false)
+                this.Text = "X";
+            if (IsMine == true)
+                this.Text = "O";
+
+            //this.Enabled = false;
+        }
+        public void Set_Flag()
+        {
+            if (this.BackColor != Color.Red)
+                this.BackColor = Color.Red;
+            //else
+            //    this.BackColor;
         }
     }
 }
